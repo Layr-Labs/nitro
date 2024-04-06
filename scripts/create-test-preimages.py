83279f6f501c1a3070a6dc37c37c8d4889493317
afkbyte <afkbyte@protonmail.com>
2024-04-06 13:28:09 -0400

    Squashed Orbit Progress Commit
    bumped arbitrum nitro to v2.3.3
    
    modify some module versions
    
    Bump EigenDA version to v0.6.1
    
    Update nitro-contracts to include fixes.
    
    fix header byte related bug
    
    feat(Arbitrator): Add support for EigenDA preimage targetting
    
    feat(Arbitrator): Add support for EigenDA preimage targetting - fmt & rm dbug stmts
    
    feat(Arbitrator): Add support for EigenDA preimage targetting - fmt & rm dbug stmts
    
    init so can debug
    
    commit and reinstall the repo
    
    bump kzg library version, new encoding issues aaa
    
    generates proof now but in a really gross way, probably incorrect
    
    update rest to use updated
    
    hash now matches preimage oracle hash
    
    serialize uncompressed bytes, not compressed
    
    rename variables appropriately
    
    the sanity checks work now holy shit
    
    clean up
    
    update submodules
    
    Update submodules to point to private develop branches
    
    update submodules
    
    rm use of bn256 kzg deps
    
    rm use of bn256 kzg deps
    
    Set submodule versions correctly, fix go dependency conflicts
    
    Revert brotli submodule to correct version
    
    Update contracts repo version

--- scripts/create-test-preimages.py
+++ scripts/create-test-preimages.py
@@ -7,6 +7,9 @@ BYTES_PER_FIELD_ELEMENT = 32
 FIELD_ELEMENTS_PER_BLOB = 4096
 KZG_ENDIANNESS='big'
 
+BN254_BLS_MODULUS = 21888242871839275222246405745257275088548364400416034343698204186575808495617
+EIGENDA_FIELD_ELEMENTS_PER_BLOB = 65536
+
 def write_data_to_file(filename, preimages):
     with open(filename, 'wb') as file:
         for preimage in preimages:
@@ -24,6 +27,20 @@ def kzg_test_data():
         data.extend(h)
     return bytes(data)
 
+def eigen_test_data():
+    data = []
+    # generate a 32 byte blob
+    for i in range(0, 1):
+        bytes_64 = bytearray(hashlib.sha512(bytes(str(i), encoding='utf8')).digest())
+        bytes_32 = bytes_64[0:32]
+
+        # 0 padding for 1st byte of 32 byte word
+        bytes_32[0] = 0
+        data.extend(bytes_32)
+
+    print(bytes(data))
+    return bytes(data)
+
 if len(sys.argv) < 2:
     print("Usage: python3 create-test-preimages.py <filename>")
     sys.exit(1)
@@ -34,6 +51,7 @@ preimages = [
     (0, b'hello world'),
     (1, b'hello world'),
     (2, kzg_test_data()),
+    (3, eigen_test_data())
 ]
 
 write_data_to_file(filename, preimages)
