97167dfa342ffe911131c6ae54f408ac4c233ff6
hopeyen <hopey1118@gmail.com>
2024-08-07 19:07:28 -0500

    fix: uncomment previously failing arbitrator test_crate test

--- arbitrator/jit/src/test.rs
+++ arbitrator/jit/src/test.rs
@@ -3,21 +3,21 @@
 
 #![cfg(test)]
 
-// use wasmer::{imports, Instance, Module, Store, Value};
+use wasmer::{imports, Instance, Module, Store, Value};
 
-// #[test]
-// fn test_crate() -> eyre::Result<()> {
-//     // Adapted from https://docs.rs/wasmer/3.1.0/wasmer/index.html
+#[test]
+fn test_crate() -> eyre::Result<()> {
+    // Adapted from https://docs.rs/wasmer/3.1.0/wasmer/index.html
 
-//     let source = std::fs::read("programs/pure/main.wat")?;
+    let source = std::fs::read("programs/pure/main.wat")?;
 
-//     let mut store = Store::default();
-//     let module = Module::new(&store, source)?;
-//     let imports = imports! {};
-//     let instance = Instance::new(&mut store, &module, &imports)?;
+    let mut store = Store::default();
+    let module = Module::new(&store, source)?;
+    let imports = imports! {};
+    let instance = Instance::new(&mut store, &module, &imports)?;
 
-//     let add_one = instance.exports.get_function("add_one")?;
-//     let result = add_one.call(&mut store, &[Value::I32(42)])?;
-//     assert_eq!(result[0], Value::I32(43));
-//     Ok(())
-// }
+    let add_one = instance.exports.get_function("add_one")?;
+    let result = add_one.call(&mut store, &[Value::I32(42)])?;
+    assert_eq!(result[0], Value::I32(43));
+    Ok(())
+}
