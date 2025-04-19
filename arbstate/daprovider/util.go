676895edd1fae09c9fc29f499c2b21e164cb9b9e
Ethen Pociask <ethen@eigenlabs.org>
2025-04-19 02:37:49 -0500

    feat: Insecure EigenDA V2 integration: remove unnecesssary comments && dbug stmts

--- arbstate/daprovider/util.go
+++ arbstate/daprovider/util.go
@@ -221,18 +221,7 @@ func RecoverPayloadFromDasBatch(
 		return nil, nil
 	}
 
-	// eigenda cert
-	// inbox submission block #
-	// reference block #
-	// if reference block # + ALLOWED_SUBMISSION_BUFFER < inbox submission block # {
-    //        INVALIDATE
-	// }
-
-	maxTimestamp := binary.BigEndian.Uint64(sequencerMsg[8:16])
-	// maxTS
-
-	// maxTS +
-	// timeout = actual UTC timestamp that cert will be invalidated  
+	maxTimestamp := binary.BigEndian.Uint64(sequencerMsg[8:16]) 
 	if cert.Timeout < maxTimestamp+MinLifetimeSecondsForDataAvailabilityCert {
 		log.Error("Data availability cert expires too soon", "err", "")
 		return nil, nil
