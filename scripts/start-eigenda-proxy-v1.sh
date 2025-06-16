de954a27f2026c06ce13e5a6dd486525a0457929
Ethen Pociask <ethen@eigenlabs.org>
2025-06-16 02:04:27 -0400

    chore: Update proxy containers to use latest v1.8.2

--- scripts/start-eigenda-proxy-v1.sh
+++ scripts/start-eigenda-proxy-v1.sh
@@ -18,6 +18,7 @@ docker run -d --name eigenda-proxy-nitro-test-instance-v1 \
   -e EIGENDA_PROXY_MEMSTORE_EXPIRATION=120m \
   -e EIGENDA_PROXY_EIGENDA_ETH_RPC=http://localhost:6969 \
   -e EIGENDA_PROXY_EIGENDA_SERVICE_MANAGER_ADDR="0x0000000000000000000000000000000000000000" \
+  -e EIGENDA_PROXY_EIGENDA_V2_DISPERSER_RPC=disperser-holesky.eigenda.xyz:443 \
   -e EIGENDA_PROXY_EIGENDA_CERT_VERIFICATION_DISABLED=true \
   ghcr.io/layr-labs/eigenda-proxy:v1.8.2
 
--- scripts/start-eigenda-proxy-v2.sh
+++ scripts/start-eigenda-proxy-v2.sh
@@ -20,6 +20,7 @@ docker run -d --name eigenda-proxy-nitro-test-instance-v2 \
   -e EIGENDA_PROXY_EIGENDA_CERT_VERIFICATION_DISABLED=true \
   -e EIGENDA_PROXY_EIGENDA_READ_G2_POINTS=true \
   -e EIGENDA_PROXY_EIGENDA_V2_DISPERSER_RPC=disperser-holesky.eigenda.xyz:443 \
+  -e EIGENDA_PROXY_EIGENDA_V2_DISPERSER_RPC=disperser-holesky.eigenda.xyz:443 \
   -e EIGENDA_PROXY_EIGENDA_V2_SIGNER_PRIVATE_KEY_HEX=ca9cfa217a7684f494a5709bc04fc57cb6051911454cb76fba2058ca0ba09552 \
   ghcr.io/layr-labs/eigenda-proxy:v1.8.2
 
