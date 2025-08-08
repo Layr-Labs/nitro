a8c00cd6b15a68d45c56a4347a4be057894c9e7b
ethenotethan <ethen@eigenlabs.org>
2025-08-08 20:50:14 +0700

    feat: Make EigenDA enablement stateless

--- arbnode/node.go
+++ arbnode/node.go
@@ -631,7 +631,7 @@ func createNodeImpl(
 	}
 
 	var dapReaders []daprovider.Reader
-	if eigenDAReader != nil {
+	if config.EigenDA.Enable {
 		dapReaders = append(dapReaders, eigenda.NewReaderForEigenDA(eigenDAReader))
 	}
 	if daReader != nil {
