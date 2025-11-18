7d5f4bf68a4558533cc3ffff2394918c65aafb7e
Shyam Patel <shyampkira@gmail.com>
2025-11-17 17:55:24 -0800

    Update EigenDA reader to use v3.9.0 Promise API pattern

--- eigenda/reader.go
+++ eigenda/reader.go
@@ -26,43 +26,37 @@ func (d *readerForEigenDA) IsValidHeaderByte(ctx context.Context, headerByte byt
 }
 
 // CollectPreimages collects preimages from the DA provider
+// TODO: Updated to v3.9.0 Promise API pattern (containers.DoPromise). Revert to old pattern if this breaks:
+//   promise, ctx := containers.NewPromiseWithContext[daprovider.PreimagesResult](context.Background())
+//   go func() { ... promise.ProduceError(err) / promise.Produce(result) }()
 func (b *readerForEigenDA) CollectPreimages(
 	batchNum uint64,
 	batchBlockHash common.Hash,
 	sequencerMsg []byte,
 ) containers.PromiseInterface[daprovider.PreimagesResult] {
-	promise, ctx := containers.NewPromiseWithContext[daprovider.PreimagesResult](context.Background())
-	go func() {
+	return containers.DoPromise(context.Background(), func(ctx context.Context) (daprovider.PreimagesResult, error) {
 		var preimages daprovider.PreimagesMap
 		var preimageRecorder daprovider.PreimageRecorder
 		preimages = make(daprovider.PreimagesMap)
 		preimageRecorder = daprovider.RecordPreimagesTo(preimages)
 
 		_, err := RecoverPayloadFromEigenDABatch(ctx, sequencerMsg[sequencerMsgOffset:], b.readerEigenDA, preimageRecorder)
-		if err != nil {
-			promise.ProduceError(err)
-		} else {
-			promise.Produce(daprovider.PreimagesResult{Preimages: preimages})
-		}
-	}()
-	return promise
+		return daprovider.PreimagesResult{Preimages: preimages}, err
+	})
 }
 
+// TODO: Updated to v3.9.0 Promise API pattern (containers.DoPromise). Revert to old pattern if this breaks:
+//   promise, ctx := containers.NewPromiseWithContext[daprovider.PayloadResult](context.Background())
+//   go func() { ... promise.ProduceError(err) / promise.Produce(result) }()
 func (d *readerForEigenDA) RecoverPayload(
 	batchNum uint64,
 	batchBlockHash common.Hash,
 	sequencerMsg []byte,
 ) containers.PromiseInterface[daprovider.PayloadResult] {
-	promise, ctx := containers.NewPromiseWithContext[daprovider.PayloadResult](context.Background())
-	go func() {
+	return containers.DoPromise(context.Background(), func(ctx context.Context) (daprovider.PayloadResult, error) {
 		payload, err := RecoverPayloadFromEigenDABatch(ctx, sequencerMsg[sequencerMsgOffset:], d.readerEigenDA, nil)
-		if err != nil {
-			promise.ProduceError(err)
-		} else {
-			promise.Produce(daprovider.PayloadResult{Payload: payload})
-		}
-	}()
-	return promise
+		return daprovider.PayloadResult{Payload: payload}, err
+	})
 }
 
 func RecoverPayloadFromEigenDABatch(ctx context.Context,
